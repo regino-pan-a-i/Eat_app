13a47718bd28ee50d79cdabf4f1120882c67ee24
Andre Regino <reginopanai@gmail.com>
2023-03-20 16:23:27 -0600

    updating list in second fragment

--- app/src/main/java/com/example/eatdraft/SecondFragment.java
+++ app/src/main/java/com/example/eatdraft/SecondFragment.java
@@ -4,12 +4,16 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
 
 import com.example.eatdraft.databinding.FragmentSecondBinding;
+
+import java.util.ArrayList;
 
 public class SecondFragment extends Fragment {
 
@@ -36,6 +40,21 @@ public class SecondFragment extends Fragment {
                         .navigate(R.id.action_SecondFragment_to_FirstFragment);
             }
         });
+        // Initialize listView here after view has been created
+        ListView listView = view.findViewById(R.id.listView);
+        // Use listView here if needed
+        ArrayList<String> items = new ArrayList<>();
+        ArrayAdapter<String> adapter;
+
+        items.add("apple");
+        items.add("manzana");
+        items.add("estoy cansado");
+        items.add("tengo hambre");
+        items.add("aiuuuuraaaa");
+
+        adapter = new ArrayAdapter<>(getActivity().getApplicationContext(), android.R.layout.simple_list_item_1, items);
+
+        listView.setAdapter(adapter);
     }
 
     @Override
