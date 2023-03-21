c7e24158077d28e89c0bf453eac6ad4fe42b0b50
Andre Regino <reginopanai@gmail.com>
2023-03-20 20:42:48 -0600

    adding and removing items from list in fragment second fixed and given a layout format

--- app/src/main/java/com/example/eatdraft/SecondFragment.java
+++ app/src/main/java/com/example/eatdraft/SecondFragment.java
@@ -4,8 +4,12 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -18,6 +22,15 @@ import java.util.ArrayList;
 public class SecondFragment extends Fragment {
 
     private FragmentSecondBinding binding;
+
+    private static ListView listView;
+    private static ArrayList<String> items;
+    private static ListViewAdapter adapter;
+    EditText input;
+    ImageView enter;
+
+
+
 
     @Override
     public View onCreateView(
@@ -40,11 +53,13 @@ public class SecondFragment extends Fragment {
                         .navigate(R.id.action_SecondFragment_to_FirstFragment);
             }
         });
-        // Initialize listView here after view has been created
-        ListView listView = view.findViewById(R.id.listView);
-        // Use listView here if needed
-        ArrayList<String> items = new ArrayList<>();
-        ArrayAdapter<String> adapter;
+
+        listView = view.findViewById(R.id.listView);
+        items = new ArrayList<>();
+
+        input = view.findViewById(R.id.input);
+        enter = view.findViewById(R.id.add_button);
+
 
         items.add("apple");
         items.add("manzana");
@@ -52,11 +67,54 @@ public class SecondFragment extends Fragment {
         items.add("tengo hambre");
         items.add("aiuuuuraaaa");
 
-        adapter = new ArrayAdapter<>(getActivity().getApplicationContext(), android.R.layout.simple_list_item_1, items);
+        adapter = new ListViewAdapter(getActivity().getApplicationContext(), items);
+        listView.setAdapter(adapter);
 
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                String name = items.get(i);
+                makeToast(name);
+
+            }
+        });
+
+        enter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String text = input.getText().toString();
+                if (text == null || text.length() == 0){
+                    makeToast("Enter new item");
+                } else{
+                    addItem(text);
+                    input.setText("");
+                    makeToast("Added: " + text);
+                }
+            }
+        });
+
+
+
+    }
+
+    public static void addItem(String item){
+        items.add(item);
         listView.setAdapter(adapter);
     }
 
+    public static void removeItem(int item){
+        items.remove(item);
+        listView.setAdapter(adapter);
+    }
+
+    Toast toast;
+    public void makeToast(String string) {
+        if (toast != null) toast.cancel();
+
+        toast = Toast.makeText(getActivity().getApplicationContext(), string, Toast.LENGTH_SHORT);
+        toast.show();
+
+    }
     @Override
     public void onDestroyView() {
         super.onDestroyView();
